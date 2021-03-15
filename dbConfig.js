f91857fe2786c476561a9b2519ed33e698ace13f
kl4532 <kornellukasiak@gmail.com>
2021-03-15 17:21:40 +0100

    test remote db connection

--- dbConfig.js
+++ dbConfig.js
@@ -1,8 +1,16 @@
+// mysql://[username]:[password]@[host]/[database name]?reconnect=true
+const dbUri =  process.env.CLEARDB_DATABASE_URL;
+const credentials = dbUri.split(":");
+const user = credentials[0];
+const password = credentials[1];
+const host = credentials[2];
+const dbName = credentials[3];
+
 const db = {
-    host: 'localhost',
-        user: 'root',
-    password: 'test',
-    database: 'Cookbook'
+    host: host || 'localhost',
+        user: user || 'root',
+    password: password || 'test',
+    database: dbName ||'Cookbook'
 }
 
 module.exports = db;
